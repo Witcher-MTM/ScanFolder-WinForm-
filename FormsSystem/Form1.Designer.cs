0f87878991ce509d9694ae04e604d9969c6979a2
babek <student@24-02>
2021-12-02 14:13:04 +0200

    Добавьте файлы проекта.

---
+++ FormsSystem/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿
+using System;
+
+namespace FormsSystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.TextBoxWhere = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.BtnWhere = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TextBoxPattern = new System.Windows.Forms.TextBox();
+            this.btnPattern = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // TextBoxWhere
+            // 
+            this.TextBoxWhere.Location = new System.Drawing.Point(12, 29);
+            this.TextBoxWhere.Name = "TextBoxWhere";
+            this.TextBoxWhere.Size = new System.Drawing.Size(217, 20);
+            this.TextBoxWhere.TabIndex = 0;
+            this.TextBoxWhere.Click += new System.EventHandler(this.TextBoxWhere_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Where scan";
+            // 
+            // BtnWhere
+            // 
+            this.BtnWhere.Location = new System.Drawing.Point(235, 8);
+            this.BtnWhere.Name = "BtnWhere";
+            this.BtnWhere.Size = new System.Drawing.Size(22, 23);
+            this.BtnWhere.TabIndex = 2;
+            this.BtnWhere.Text = "🔎";
+            this.BtnWhere.UseVisualStyleBackColor = true;
+            this.BtnWhere.Click += new System.EventHandler(this.BtnWhere_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Pattern of search";
+            // 
+            // TextBoxPattern
+            // 
+            this.TextBoxPattern.Location = new System.Drawing.Point(12, 90);
+            this.TextBoxPattern.Name = "TextBoxPattern";
+            this.TextBoxPattern.Size = new System.Drawing.Size(217, 20);
+            this.TextBoxPattern.TabIndex = 4;
+            this.TextBoxPattern.Click += new System.EventHandler(this.TextBoxPattern_Click);
+            // 
+            // btnPattern
+            // 
+            this.btnPattern.Location = new System.Drawing.Point(261, 90);
+            this.btnPattern.Name = "btnPattern";
+            this.btnPattern.Size = new System.Drawing.Size(54, 23);
+            this.btnPattern.TabIndex = 5;
+            this.btnPattern.Text = "Search";
+            this.btnPattern.UseVisualStyleBackColor = true;
+            this.btnPattern.Click += new System.EventHandler(this.btnPattern_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(15, 117);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(773, 316);
+            this.listBox1.TabIndex = 6;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "in Name",
+            "in Ext",
+            "in date creation",
+            "in last writing",
+            "in last open"});
+            this.checkedListBox1.Location = new System.Drawing.Point(463, 8);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(120, 94);
+            this.checkedListBox1.TabIndex = 7;
+            this.checkedListBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.checkedListBox1_MouseClick);
+            this.checkedListBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.checkedListBox1_MouseDoubleClick);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(588, 39);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 8;
+            this.dateTimePicker1.Visible = false;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseClick);
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            this.notifyIcon1.ContextMenuStrip = new System.Windows.Forms.ContextMenuStrip();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnPattern);
+            this.Controls.Add(this.TextBoxPattern);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.BtnWhere);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TextBoxWhere);
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.Text = "Form1";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+       
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TextBoxWhere;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button BtnWhere;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TextBoxPattern;
+        private System.Windows.Forms.Button btnPattern;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ FormsSystem/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormsSystem
+{
+    public partial class Form1 : Form
+    {
+       
+        public Form1()
+        {
+            InitializeComponent();
+            this.notifyIcon1.ContextMenuStrip.Items.Add("Close", null, this.MenuCloseClick);
+            this.notifyIcon1.ContextMenuStrip.Items.Add("Show", null, this.MenuShowClick);
+        }
+
+       
+
+        private void TextBoxWhere_Click(object sender, EventArgs e)
+        {
+            this.TextBoxWhere.Text = "";
+        }
+
+        private void TextBoxPattern_Click(object sender, EventArgs e)
+        {
+            this.TextBoxPattern.Text = "";
+        }
+
+        private void BtnWhere_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog file = new OpenFileDialog())
+            {
+                if (file.ShowDialog() == DialogResult.OK)
+                {
+                    this.TextBoxWhere.Text = Path.GetDirectoryName(file.FileName);
+                }
+
+            }
+        }
+
+        private void btnPattern_Click(object sender, EventArgs e)
+        {
+            this.listBox1.Items.Clear();
+
+
+            if (this.TextBoxPattern.Text.Length > 0 && this.TextBoxWhere.Text.Length > 0)
+            {
+                if (this.checkedListBox1.CheckedItems.Count > 0)
+                {
+
+                    foreach (var item in this.checkedListBox1.CheckedItems)
+                    {
+                        foreach (var dir in Directory.GetFiles(this.TextBoxWhere.Text))
+                        {
+                            if (item.ToString().ToLower().Contains("name"))
+                            {
+
+                                if (Path.GetFileName(dir).Contains(this.TextBoxPattern.Text))
+                                {
+                                    this.listBox1.Items.Add(Path.GetFileName(dir));
+                                }
+                            }
+                            if (item.ToString().ToLower().Contains("date"))
+                            {
+
+                                if (File.GetCreationTime(dir).Date.ToShortDateString() == this.dateTimePicker1.Value.Date.ToShortDateString())
+                                {
+                                    this.listBox1.Items.Add(Path.GetFileName(dir));
+                                }
+                            }
+                            if (item.ToString().ToLower().Contains("ext"))
+                            {
+
+                                if (Path.GetExtension(dir).Contains(this.TextBoxPattern.Text))
+                                {
+                                    this.listBox1.Items.Add(Path.GetFileName(dir));
+                                }
+                            }
+                            if (item.ToString().ToLower().Contains("open"))
+                            {
+
+                                if (File.GetLastAccessTime(dir).Date.ToShortDateString() == this.dateTimePicker1.Value.Date.ToShortDateString())
+                                {
+                                    this.listBox1.Items.Add(Path.GetFileName(dir));
+                                }
+                            }
+                            if (item.ToString().ToLower().Contains("write"))
+                            {
+
+                                if (File.GetLastWriteTime(dir).ToShortDateString() == this.dateTimePicker1.Value.Date.ToShortDateString())
+                                {
+                                    this.listBox1.Items.Add(Path.GetFileName(dir));
+                                }
+                            }
+                        }
+                        GC.Collect();
+
+                    }
+
+                }
+                else
+                {
+                    foreach (var item in Directory.GetFiles(this.TextBoxWhere.Text))
+                    {
+                        if (Path.GetFileName(item).Contains(this.TextBoxPattern.Text))
+                        {
+                            this.listBox1.Items.Add(Path.GetFileName(item));
+                        }
+                        if (this.listBox1.Items.Count <= 0)
+                        {
+                            MessageBox.Show("No one found");
+                        }
+                        GC.Collect();
+                    }
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Fill all text");
+            }
+        }
+
+        private void checkedListBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            for (int i = 0; i < this.checkedListBox1.Items.Count; i++)
+            {
+                if (checkedListBox1.Items[i].ToString().ToLower().Contains("in date") || checkedListBox1.Items[i].ToString().ToLower().Contains("in last"))
+                {
+                    this.dateTimePicker1.Visible = true;
+                }
+            }
+        }
+
+        private void checkedListBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            for (int i = 0; i < this.checkedListBox1.Items.Count; i++)
+            {
+                if (checkedListBox1.Items[i].ToString().ToLower().Contains("in date") || checkedListBox1.Items[i].ToString().ToLower().Contains("in last"))
+                {
+                    this.dateTimePicker1.Visible = true;
+                    this.TextBoxPattern.Text = "If you need->enter pattern";
+                }
+            }
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            this.TextBoxPattern.Text = this.dateTimePicker1.Value.Date.ToShortDateString();
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+                notifyIcon1.Visible = true;
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            this.WindowState = FormWindowState.Normal;
+            notifyIcon1.Visible = false;
+        }
+
+        private void notifyIcon1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                
+            }
+        }
+      
+        private void MenuShowClick(object sender, EventArgs e)
+        {
+            Show();
+            this.WindowState = FormWindowState.Normal;
+            notifyIcon1.Visible = false;
+        }
+        private void MenuCloseClick(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
